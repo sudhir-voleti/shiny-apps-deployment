53c61220decd7e5b2741adbb6e2fdf5155221fd1
Aashish Pandey <aashishkpandey@gmail.com>
2016-07-24 19:39:19 +0530

    added dependency

---
+++ dependency-factor-analysis-shinyapp.R
@@ -0,0 +1,7 @@
+try(require(shiny)||install.packages("shiny"))
+try(require(nFactors)||install.packages("nFactors"))
+try(require(qgraph)||install.packages("qgraph"))
+
+library("shiny")
+library("nFactors")
+library("qgraph")
