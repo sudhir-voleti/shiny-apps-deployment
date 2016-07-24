7eaa5c6f1bbe26bc6ef25a8fadbb2a1e63e50b6e
Aashish Pandey <aashishkpandey@gmail.com>
2016-07-24 19:41:03 +0530

    edited dependency

--- dependency-factor-analysis-shinyapp.R
+++ dependency-factor-analysis-shinyapp.R
@@ -1,6 +1,6 @@
-try(require(shiny)||install.packages("shiny"))
-try(require(nFactors)||install.packages("nFactors"))
-try(require(qgraph)||install.packages("qgraph"))
+try(require("shiny")||install.packages("shiny"))
+try(require("nFactors")||install.packages("nFactors"))
+try(require("qgraph")||install.packages("qgraph"))
 
 library("shiny")
 library("nFactors")
